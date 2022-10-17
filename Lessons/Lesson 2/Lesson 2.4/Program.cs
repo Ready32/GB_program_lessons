dc9ea794259acf0acf57d0a3f5b1a5b6bcece744
Ready32 <amelin-free@yandex.ru>
2022-10-17 22:55:30 +0300

    done lesson 2.4

--- Lessons/Lesson 2/Lesson 2.4/Program.cs
+++ Lessons/Lesson 2/Lesson 2.4/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 9 //
+System.Console.Write("Для проверки кратности числам 7 и 23 введите число = ");
+int n = int.Parse(Console.ReadLine());
+int num1 = 7, num2 = 23;
+System.Console.Write("Задача №9 - ");
+if (n % 7 == 0 && n % 23 == 0)
+{
+    System.Console.WriteLine("Число " + n + " - кратно числам 7 и 23 одновременно.");
+}
+else
+{
+    System.Console.WriteLine("Число " + n + " - не кратно числам 7 и 23 одновременно.");
+}
+
