7096b5fde68be0df520c3966d6baa0cd3f87f20e
Ready32 <amelin-free@yandex.ru>
2022-10-17 23:17:07 +0300

    done lesson 2.2

--- Lessons/Lesson 2/Lesson 2.2/Program.cs
+++ Lessons/Lesson 2/Lesson 2.2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13 //
+
+System.Console.Write("Введите число, 3-ю цифру которого необходимо вывести = ");
+string s = Console.ReadLine();
+char[] str = s.ToCharArray();
+Array.Reverse(str);
+int a = int.Parse(str);
+System.Console.Write("Задача №13 - ");
+if (a > 99)
+{
+    Console.WriteLine((a / 100) % 10);
+}
+else
+{
+    System.Console.WriteLine("Третье цифры нет");
+}
