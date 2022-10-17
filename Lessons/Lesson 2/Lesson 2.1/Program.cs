f5942a823ab05eafbb490e3212a2628c951dcb4b
Ready32 <amelin-free@yandex.ru>
2022-10-17 22:52:39 +0300

    done lesson 2.1

--- Lessons/Lesson 2/Lesson 2.1/Program.cs
+++ Lessons/Lesson 2/Lesson 2.1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10 //
+
+System.Console.Write("Задача №10 - ");
+int a = 782;
+int LastNum = a % 100;
+int MiddleNum = LastNum / 10;
+System.Console.WriteLine(MiddleNum);
