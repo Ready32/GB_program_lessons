c8aa573a0a08e37d80493ba8f3a86ae44f0915e4
Ready32 <amelin-free@yandex.ru>
2022-10-17 22:53:22 +0300

    done lesson 2.3

--- Lessons/Lesson 2/Lesson 2.3/Program.cs
+++ Lessons/Lesson 2/Lesson 2.3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15//
+System.Console.Write("Задача №15 - ");
+int c = 3;
+if (c <= 5)
+{
+    System.Console.WriteLine("Будний день");
+}
+else
+{
+    System.Console.WriteLine("Выходной");
+}
