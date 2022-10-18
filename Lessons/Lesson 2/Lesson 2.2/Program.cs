941841296437cd3a189c4b0c1dde88ccaee8cc00
Ready32 <amelin-free@yandex.ru>
2022-10-18 20:48:46 +0300

    Update lesson 2.2(ex.13)

--- Lessons/Lesson 2/Lesson 2.2/Program.cs
+++ Lessons/Lesson 2/Lesson 2.2/Program.cs
@@ -3,14 +3,4 @@
 System.Console.Write("Введите число, 3-ю цифру которого необходимо вывести = ");
 string s = Console.ReadLine();
 char[] str = s.ToCharArray();
-Array.Reverse(str);
-int a = int.Parse(str);
-System.Console.Write("Задача №13 - ");
-if (a > 99)
-{
-    Console.WriteLine((a / 100) % 10);
-}
-else
-{
-    System.Console.WriteLine("Третье цифры нет");
-}
+System.Console.WriteLine("Задача №13 - " + str[2]);
