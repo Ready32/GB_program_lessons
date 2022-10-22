b3f1b2fc9adde415ae28e164eae7846e733dde21
Ready32 <amelin-free@yandex.ru>
2022-10-22 14:28:51 +0300

    done Lesson 3.1

---
+++ Lessons/Lesson 3/Lesson 3.1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 19 - Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.Write("Введите число для проверки на палиндромность = ");
+string number = Console.ReadLine();
+char[] CharNumber = number.ToCharArray();
+Array.Reverse(CharNumber);
+string NewNumber = new string(CharNumber);
+
+if (number == NewNumber)
+{
+    System.Console.WriteLine("Данная запись является палиндромом");
+}
+if (number != NewNumber)
+{
+    System.Console.WriteLine("Данная запись не является палиндромом");
+}
