b965392dc7541e37214c64038f55fa6249f903f3
Ready32 <amelin-free@yandex.ru>
2022-10-26 00:07:21 +0300

    done lesson 4.3

---
+++ Lessons/Lesson 4/Lesson 4.3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+System.Console.Write("Какой длины массив необходимо вывести? - ");
+int a = int.Parse(Console.ReadLine()!);
+int[] array = new int[a];
+
+for (int i = 0; i < a; i++)
+{
+    System.Console.Write("Введите [" + i + "] элемент массива = ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+string s = String.Join(", ", array);
+System.Console.Write("Ваш массив - " + "[" + s + "]");
