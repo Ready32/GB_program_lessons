3b52ea5cfe63e229cd740a192d77b03e903116bd
Ready32 <amelin-free@yandex.ru>
2022-11-14 22:42:21 +0300

    done Lesson 5.1

--- Lessons/Lesson 3/Lesson 3.4/Program.cs
+++ Lessons/Lesson 3/Lesson 3.4/Program.cs
@@ -1,20 +1,33 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу простых чисел от 1 до N.
 // 11 -> 2, 3, 5 , 7, 11
 
-// System.Console.Write("Введите число N, что бы вывести таблицу простых чисел от 1 до N = ");
-// int N = int.Parse(Console.ReadLine()!);
+System.Console.Write("Введите число N, что бы вывести таблицу простых чисел от 1 до N = ");
+int n = int.Parse(Console.ReadLine()!);
+int x = 2;
+int i = 2;
 
-// for (int i = 2; i <= N; i++)
-// {
-//     for (int k = 1; k <= 100; k++)
+while (n > x)
+{
+    while (i < n)
+    {
+        for (int a = 2; a < n; a++)
+        {
+            if (n % a == 0)
+            {
+                System.Console.WriteLine(n + " - не простое");
+                n--;
+            }
+            else
+            {
+                System.Console.WriteLine(n + " - простое");
+                n--;
+            }
+        }
 
-//         if (i % k == 0)
-//         {
-//             int result = 0;
-//             result++;
-//             if (result <= 2)
-//             {
-//                 System.Console.WriteLine(i);
-//             }
-//         }
-// }
+
+        i++;
+    }
+    n--;
+    System.Console.WriteLine(i);
+
+}
---
+++ Lessons/Lesson 5/Lesson 5.1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//[345, 897, 568, 234] -> 2
+
+
+Console.Write("Введите размерность массива = ");
+int[] arr = new int[int.Parse(Console.ReadLine())];
+int result = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    Random rand = new Random();
+    int num = rand.Next(100, 999);
+    int resultNum = num;
+    arr[i] = resultNum;
+
+    if (arr[i] % 2 == 0)
+    {
+        result++;
+    }
+}
+
+string s = String.Join(", ", arr);
+System.Console.Write("Ваш массив - " + "[" + s + "]");
+System.Console.WriteLine();
+System.Console.WriteLine("Всего чётных элементов в массиве - " + result);
