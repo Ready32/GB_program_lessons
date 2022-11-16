3d6be5bc4a56498d2c606cf2c5be602c12f77ad2
Ready32 <amelin-free@yandex.ru>
2022-11-17 00:21:18 +0300

    done Lesson 6.1

---
+++ Lessons/Lesson 6/Lesson 6.1/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+System.Console.Write("Сколько чисел будем вводить? - ");
+int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+int result = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write("Введите число №" + (i + 1) + " - ");
+    double num = double.Parse(Console.ReadLine());
+    array[i] = Convert.ToInt32(num);
+
+}
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        result = result + 1;
+    }
+}
+
+System.Console.WriteLine("В вашем наборе " + result + " - элемент(a)(ов) больше 0");
