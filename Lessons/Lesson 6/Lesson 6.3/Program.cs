de14a0d11ebe54fd69ef3ea07296012e898b050a
Ready32 <amelin-free@yandex.ru>
2022-11-17 02:01:23 +0300

    done Lesson 6.3

---
+++ Lessons/Lesson 6/Lesson 6.3/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Дополнительно. Дан массив. Найдите MEX массива. MEX (Minimum EXcluded) - минимальный отсутствующий элемент. */
+
+/* MEX
+1. Ищем минимальное число в массиве
+2. Ищем есть ли (минимальное число + 1)
+3. Если нет, то MEX = (минимальное число + 1)
+Иначе перезаписываем минимальное число и повторяем алгоритм */
+
+/* 0   8
+3   6
+2   1
+4   4
+2   4
+3   2
+0   3 */
+
+
+int[] array = new int[4] { 5, 2, 3, 1 };
+int mex = array[0];
+int result;
+
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] < mex)
+    {
+        mex = array[i];
+
+    }
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == mex + 1)
+    {
+        mex = mex + 1;
+    }
+
+
+}
+System.Console.WriteLine("MEX = " + (mex + 1));
