32dcc12cadf762719bf3eedcecd6b024b1797caa
Ready32 <amelin-free@yandex.ru>
2022-11-20 12:06:48 +0300

    done Lesson 7.1

---
+++ Lessons/Lesson 7/Lesson 7.1/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9
+ */
+
+double[,] array = GetArrayRandom(3, 4);
+PrintArray(array);
+
+
+double[,] GetArrayRandom(int rows, int cols, int minValue = 0, int maxValue = 10)
+{
+    double[,] array = new double[rows, cols];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
