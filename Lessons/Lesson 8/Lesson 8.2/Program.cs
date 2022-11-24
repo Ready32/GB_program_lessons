372774bf9566c6d3a2d62f78b5da3696f7e6fcea
Ready32 <amelin-free@yandex.ru>
2022-11-25 01:05:26 +0300

    done Lesson8.2

--- Lessons/Lesson 8/Lesson 8.2/Program.cs
+++ Lessons/Lesson 8/Lesson 8.2/Program.cs
@@ -13,9 +13,66 @@
 Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
  */
 
+System.Console.Write("Введите количество строк массива = ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов массива = ");
+int b = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[a, b];
+array = GetArrayRandom(a, b);
+System.Console.WriteLine("Ваш массив");
+PrintArray(array);
+int minsum = Int32.MaxValue;
+int indexLine = 0;
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        sum = sum + array[i, j];
+    }
+    if (sum < minsum)
+    {
+        minsum = sum;
+        indexLine++;
+    }
+}
+System.Console.WriteLine("Строка № " + indexLine + " имеет наименьшую сумму элементов");
+System.Console.WriteLine("Сумма элементов в строке = " + minsum);
 
 
-int[,] array = new int[4, 4] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 }, { 5, 2, 6, 7 } };
+int[,] GetArrayRandom(int a, int b, int minValue = 0, int maxValue = 20)
+{
+    int[,] array = new int[a, b];
+    int count = a * b;
+
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+
+
+/* int[,] array = new int[4, 4] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 }, { 5, 2, 6, 7 } };
 PrintArray(array);
 
 int sum1 = 0;
@@ -67,4 +124,4 @@ void PrintArray(int[,] array)
         }
         System.Console.WriteLine();
     }
-}
+} */
