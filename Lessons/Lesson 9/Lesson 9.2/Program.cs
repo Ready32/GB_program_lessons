a976768c9e5186117e2237289b279399c7f15be5
Ready32 <amelin-free@yandex.ru>
2022-11-26 11:20:15 +0300

    done Lesson 9.2

---
+++ Lessons/Lesson 9/Lesson 9.2/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+System.Console.Write("Введите число от которого будем считать сумму = ");
+int n = Int32.Parse(Console.ReadLine());
+System.Console.Write("Введите число до которого будем считать сумму = ");
+int m = Int32.Parse(Console.ReadLine());
+
+int min = 0;
+int max = 0;
+
+if (n < m)
+{
+    min = n;
+    max = m;
+}
+else
+{
+    min = m;
+    max = n;
+}
+
+void Rec(int min, int max)
+{
+    int sum = 0;
+
+
+    while (max >= min)
+    {
+        sum += max;
+        max--;
+    }
+
+    System.Console.Write($"{sum}");
+
+    return;
+}
+
+
+Rec(min, max);
+System.Console.Write($" - Сумма элементов от {min} до {max}");
